3a93dbf54e03b502369c6088aefdcb2578f5ec67
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-04-20 20:38:48 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -20,10 +20,10 @@ const server = http.createServer((req, res) => {
         }
         if(result === 'even'){
           res.writeHead(200, { 'Content-Type': 'text/plain' });
-          res.end(`'The number ${value} is even'`);
+          res.end(`The number ${value} is even`);
         } else{
           res.writeHead(404, { 'Content-Type': 'text/plain' });
-          res.end(`'The number ${value} is odd'`);
+          res.end(`The number ${value} is odd`);
         }
       } else {
         res.writeHead(400, { 'Content-Type': 'text/plain' });
